405753d16cb276223928ccabb18b7a2816da5e10
Anju Biju <anjuammus03@gmail.com>
2021-09-21 07:07:43 +0530

    Updated

--- src/test/java/utilities/WaitUtil.java
+++ src/test/java/utilities/WaitUtil.java
@@ -20,48 +20,4 @@ public class WaitUtil extends BaseClass {
 
     }
 
-    public List<WebElement> visibilityOfAllElements(By locator, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
-    }
-
-    public WebElement waitForElementToBeLocated(By locator, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-    }
-
-    public WebElement waitForElementToBeVisible(By locator, int timeOut){
-        WebElement element = driver.findElement(locator);
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        return wait.until(ExpectedConditions.visibilityOf(element));
-    }
-
-    public String waitForPageTitlePresent(String titleValue, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        wait.until(ExpectedConditions.titleContains(titleValue));
-        return driver.getTitle();
-    }
-
-    public Alert waitForAlertToBePresent(int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        return wait.until(ExpectedConditions.alertIsPresent());
-    }
-
-    public boolean waitForUrl(String url, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        return wait.until(ExpectedConditions.urlContains(url));
-    }
-
-    public void clickWhenReady(By locator, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
-        element.click();
-    }
-    public void waitForFrame(By locator, int timeOut){
-        WebDriverWait wait = new WebDriverWait(driver, timeOut);
-         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(locator));
-
-    }
-
-
 }
